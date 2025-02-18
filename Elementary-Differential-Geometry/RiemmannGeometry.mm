11859b5fa4738f13c2b0e1187b306c9dabb1a247
rxken <rxxxie.ai@gmail.com>
2025-02-17 23:15:19 -0800

    Add Riemann geometry note

---
+++ Elementary-Differential-Geometry/RiemmannGeometry.mm
@@ -0,0 +1,197 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Geometry" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1739856760821">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8ff" zoom="1.9487174">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_nord_template.mm" show_tags="UNDER_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Misc" POSITION="bottom_or_right" ID="ID_441576317" CREATED="1739860641655" MODIFIED="1739860647462">
+<node TEXT="Intrinsic Properties" POSITION="bottom_or_right" ID="ID_634676564" CREATED="1739860034661" MODIFIED="1739860047716">
+<node TEXT="That are perserved by isometry" ID="ID_791792785" CREATED="1739860047717" MODIFIED="1739860053620"/>
+</node>
+<node TEXT="Simplex" POSITION="bottom_or_right" ID="ID_1876124712" CREATED="1739861008182" MODIFIED="1739861012755">
+<node TEXT="generalization of triangle/tetrahedron to arbitrary dimension" ID="ID_613257080" CREATED="1739861363917" MODIFIED="1739861385158">
+<node TEXT="\latex $$C = \{ \sum_k \theta_i u_i | \sum_k \theta_i = 1,  \theta_i \geq 0 \}$$ where $u_i$ are affinely independent ($\{u_i - u_0\}$ are linearly independent)" POSITION="bottom_or_right" ID="ID_760224249" CREATED="1739861013644" MODIFIED="1739861285194">
+<node TEXT="0D simplex is a point" ID="ID_830450295" CREATED="1739861305000" MODIFIED="1739861314172"/>
+<node TEXT="1D simplex is a line segment" ID="ID_1771526837" CREATED="1739861314840" MODIFIED="1739861320299"/>
+<node TEXT="2D simplex is a triangle" ID="ID_702842839" CREATED="1739861320704" MODIFIED="1739861329280"/>
+<node TEXT="3D simplex is tetrahedron" ID="ID_1012570796" CREATED="1739861329672" MODIFIED="1739861339596"/>
+<node TEXT="4D simplex is a 5-cell" ID="ID_1850677036" CREATED="1739861342140" MODIFIED="1739861347847"/>
+</node>
+</node>
+</node>
+<node TEXT="Simplicial Complex" POSITION="bottom_or_right" ID="ID_1132903373" CREATED="1739861463107" MODIFIED="1739861469254">
+<node TEXT="a set of simplices that satisfies" ID="ID_1297267159" CREATED="1739861469829" MODIFIED="1739861500342">
+<node TEXT="" ID="ID_1128604142" CREATED="1739861674251" MODIFIED="1739861674251">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Every face of a simplex from the set is also in the set" ID="ID_1237306294" CREATED="1739861501025" MODIFIED="1739861523539"/>
+<node TEXT="The non-empty intersection of any two simplices is a face of the two" ID="ID_1349469237" CREATED="1739861516509" MODIFIED="1739861551203"/>
+<node TEXT="" ID="ID_515391219" CREATED="1739861674249" MODIFIED="1739861674251">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Note: 3D mesh is also an example" ID="ID_1018716119" CREATED="1739861685803" MODIFIED="1739861728699">
+<hook URI="RiemmannGeometry_files/png-250217-225444298-13772692025660783294.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Euler characteristic" POSITION="bottom_or_right" ID="ID_1935790116" CREATED="1739860651121" MODIFIED="1739860680941">
+<node TEXT="Polyhedron formula" POSITION="bottom_or_right" ID="ID_815612447" CREATED="1739860780939" MODIFIED="1739860843534">
+<node TEXT="\latex \chi(S) = V - E + F " POSITION="bottom_or_right" ID="ID_1702695861" CREATED="1739860664732" MODIFIED="1739860719506">
+<node TEXT="png-250217-224103621-18117530150378249048.png" ID="ID_229184443" CREATED="1739860866793" MODIFIED="1739860866793">
+<hook URI="RiemmannGeometry_files/png-250217-224103621-18117530150378249048.png" SIZE="0.80862534" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="Homotopy invariant" POSITION="bottom_or_right" ID="ID_1428558478" CREATED="1739860936101" MODIFIED="1739861598377"/>
+</node>
+</node>
+<node TEXT="Euclidean Plane" POSITION="bottom_or_right" ID="ID_1942182363" CREATED="1739856761600" MODIFIED="1739856768883">
+<node TEXT="Triangle" ID="ID_886999408" CREATED="1739859508699" MODIFIED="1739859513700">
+<node TEXT="Classification Theorem" POSITION="bottom_or_right" ID="ID_880830579" CREATED="1739858515110" MODIFIED="1739859449348">
+<node TEXT="Side Side Side Theorem" POSITION="bottom_or_right" ID="ID_739794988" CREATED="1739856780483" MODIFIED="1739856788138">
+<node TEXT="Euclidean Triangles are congruent IFF their corresponding sides are equal" ID="ID_1148866942" CREATED="1739856802752" MODIFIED="1739858221958">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+<node TEXT="Local to Global theorem" POSITION="bottom_or_right" ID="ID_149092860" CREATED="1739857348044" MODIFIED="1739859447586">
+<node TEXT="Angle Sum Theorem" POSITION="bottom_or_right" ID="ID_1833949810" CREATED="1739856788824" MODIFIED="1739856843114">
+<node TEXT="\latex \textbf{Euclidean triangle interior angle sums to } ${\pi}" ID="ID_499826458" CREATED="1739856857249" MODIFIED="1739858258322">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Circle" ID="ID_138157327" CREATED="1739859529027" MODIFIED="1739859530500">
+<node TEXT="Classification Theorem" POSITION="bottom_or_right" ID="ID_791979547" CREATED="1739858515110" MODIFIED="1739859449348">
+<node TEXT="Circle Classification Theorem" POSITION="bottom_or_right" ID="ID_185659369" CREATED="1739858080658" MODIFIED="1739858094843">
+<node TEXT="Two circles are congruent if their have the same radius" ID="ID_1114268832" CREATED="1739858094845" MODIFIED="1739858108271"/>
+</node>
+</node>
+<node TEXT="Local to Global theorem" POSITION="bottom_or_right" ID="ID_926194147" CREATED="1739857348044" MODIFIED="1739859447586">
+<node TEXT="Circumference Theorem" POSITION="bottom_or_right" ID="ID_76974700" CREATED="1739858110877" MODIFIED="1739858118459">
+<node TEXT="\latex \textbf{Circumference of a circle of radius } $R$ \textbf{ is } $2\pi R$ " ID="ID_1696400967" CREATED="1739858118461" MODIFIED="1739858246607"/>
+</node>
+</node>
+</node>
+<node TEXT="Space Curve" ID="ID_806189237" CREATED="1739859537165" MODIFIED="1739859540551">
+<node TEXT="Classification Theorem" POSITION="bottom_or_right" ID="ID_96213855" CREATED="1739858515110" MODIFIED="1739859449348">
+<node TEXT="Plane Curve Classification Theorem" POSITION="bottom_or_right" ID="ID_1634123702" CREATED="1739858725023" MODIFIED="1739858735171">
+<node TEXT="Two curves defined on [a,b] are congruent if their signed curvatures are the same at each t in [a,b]" ID="ID_1468383029" CREATED="1739858773370" MODIFIED="1739859175693"/>
+</node>
+</node>
+<node TEXT="Local to Global theorem" POSITION="bottom_or_right" ID="ID_712876253" CREATED="1739857348044" MODIFIED="1739859447586">
+<node TEXT="Total Curvature Theorem" POSITION="bottom_or_right" ID="ID_1388310693" CREATED="1739858745836" MODIFIED="1739858751467">
+<node TEXT="\latex If  $\gamma$ defined on [a,b] is a unit-speed simple closed curve ($\gamma&apos;(a)=\gamma&apos;(b)$) and $N$ is inward pointing normal then $$\int_a^b \kappa_N(t)dt = 2 \pi$$" ID="ID_442292370" CREATED="1739859203794" MODIFIED="1739859331864"/>
+</node>
+</node>
+</node>
+<node TEXT="Surface" ID="ID_1275053575" CREATED="1739859640720" MODIFIED="1739859642918">
+<node TEXT="Principal Curvatures" ID="ID_1736080181" CREATED="1739859649052" MODIFIED="1739859683172">
+<node TEXT="\latex $\kappa_1, \kappa_2$ are the \min and \max of the signed curvature curvees passing $p$" ID="ID_240066301" CREATED="1739859850609" MODIFIED="1739859927137"/>
+<node TEXT="NOT intrinsic. There exists isometry between cylinder and square. Square has both principal curvatures 0 but not cylinder." ID="ID_1297713263" CREATED="1739859852454" MODIFIED="1739860141820"/>
+</node>
+<node TEXT="Gaussian Curvature" ID="ID_1840032307" CREATED="1739860150180" MODIFIED="1739860159117">
+<node TEXT="\latex $K = \kappa_1\kappa_2$" ID="ID_481081057" CREATED="1739860191756" MODIFIED="1739860614836"/>
+<node TEXT="Intrinsic" ID="ID_1742264403" CREATED="1739860213108" MODIFIED="1739860216580"/>
+</node>
+<node TEXT="Classification Theorem" ID="ID_1625734188" CREATED="1739860252805" MODIFIED="1739860261128">
+<node TEXT="Uniformization Theorem" ID="ID_1020834228" CREATED="1739860292119" MODIFIED="1739860297254">
+<node TEXT="Every connected 2-manifold is diffeomorphic to a quotient of one of the constant curvature model" ID="ID_1917355430" CREATED="1739860297636" MODIFIED="1739860352343">
+<node TEXT="Hyperbolic plane, curvature &lt; 0" ID="ID_1198728686" CREATED="1739860359553" MODIFIED="1739860380555"/>
+<node TEXT="Euclidean plane, curvature = 0" ID="ID_1461084738" CREATED="1739860382535" MODIFIED="1739860424021"/>
+<node TEXT="Sphere surface, curvature &gt; 0" ID="ID_659472827" CREATED="1739860424782" MODIFIED="1739860449441"/>
+</node>
+<node TEXT="Every connected 2 manifold has a complete Riemannian metric with constant Gaussian curvature" ID="ID_986135731" CREATED="1739860496330" MODIFIED="1739860519718"/>
+</node>
+</node>
+<node TEXT="Local to Global Theorem" ID="ID_1660614617" CREATED="1739860261577" MODIFIED="1739860267392">
+<node TEXT="Gauss-Bonnet Theorem" ID="ID_1673753361" CREATED="1739860473432" MODIFIED="1739860487476">
+<node TEXT="\latex Suppose S is a compact Riemannian 2-manifold. Then $$\int_S K dA = 2 \pi \chi(S)$$" ID="ID_1205728882" CREATED="1739860487478" MODIFIED="1739860602751"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
